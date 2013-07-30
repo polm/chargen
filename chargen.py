d926bee27d67087b9423d464385f0c2e3098580a
Paul McCann <p-mccann@m3.com>
2013-07-30 18:43:35 +0400

    Progress. -POLM

--- chargen.py
+++ chargen.py
@@ -17,8 +17,14 @@ from docopt import docopt
 from itertools import groupby
 from math import floor
 from random import random
+import gc
+from collections import Counter
 
 def has_hypernym(word, hyper):
+    # Can't use lowest common hypernym function here because it's broken
+    # A chef is a person, but their lowest common hypernym is organism (through person).
+    # It's a bug and has been fixed but not yet released.
+    hypers = wn.synsets(hyper)
     only_nouns = lambda x: x.pos == wn.NOUN
     syns = filter(only_nouns,wn.synsets(word))
     if not syns: return False
@@ -27,7 +33,7 @@ def has_hypernym(word, hyper):
         syn = syn.hypernyms()
         if not syn: return False
         else: syn = first(syn)
-        if syn in wn.synsets(hyper):
+        if syn in hypers:
             return True
     return False
 
@@ -36,57 +42,59 @@ def first(ll): return ll[0]
 def last(ll): return ll[-1]
 def lower(ss): return ss.lower()
 
-def discard_common(ll, percent):
-    """Discard first entries of a list based on percent parameters."""
-    # Smash case (making a copy in the process)
-    nl = map(lower,ll)
-    nl.sort()
-    # Get the counts of the sorted list
-    nl= zip(set(nl),[len(list(group)) for key, group in groupby(nl)])
-    nl.sort(key=last)
-    # Throw out the first bits (presumably too common)
-    return map(first, nl[int(floor(len(nl)*percent)):])
+def get_tagged_counts(fname):
+    """Return a Counter with the token/tag pairs."""
+    # Putting this in a function helps gc
+    ff = open(fname)
+    doc = ff.read()
+    ff.close()
+    sents = nltk.sent_tokenize(doc)
+    return sum([Counter(nltk.pos_tag(nltk.word_tokenize(sent))) for sent in sents],Counter())
 
 def pick(ll):
     """Pick a random element from a list"""
-    return ll[int(floor(random() * len(ll)))]
+    return ll[int(random() * len(ll))]
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Character Generator 0.1')
     print "Reading file..."
-    doc = open(arguments['<file>']).read()
-    print "Read file, tagging..."
-    sents = nltk.sent_tokenize(doc)
-    tagged = sum([nltk.pos_tag(nltk.word_tokenize(sent)) for sent in sents],[])
+    tagged = get_tagged_counts(arguments['<file>'])
     print "Tagging finished"
+    # As a lazy stop word filter, remove the most common 10% of words.
+    #tagged = tagged.most_common()[int(len(tagged.keys())/10):]
+    tagged = tagged.keys()
+    # Do some cleanup
+    tagged = set(map(lambda x: (first(x).lower(), last(x)), tagged))
+    tagged = filter(lambda x: not has_hypernym(first(x), 'number'),tagged)
+    # Necessary if plaintext tables are around
+    tagged = filter(lambda x: not '|' in first(x),tagged)
+    print tagged[0:10]
 
     # Get the adjectives
-    adjs = map(first, filter(lambda x: x[1] == 'JJ', tagged))
-    adjs = discard_common(adjs, .1)
+    adjs = map(first, filter(lambda x: last(x) == 'JJ', tagged))
+    nouns = map(first, filter(lambda x: last(x) == 'NN', tagged))
+    print nouns[0:10]
 
-    nouns = map(first, filter(lambda x: x[1] == 'NN', tagged))
+
 
     if not arguments['--event']:
         # Get the nouns that describe people
         is_person = lambda x: has_hypernym(x,'person')
-        nns = filter(is_person, nouns)
-        nns = discard_common(nns, .2)
+        people = filter(is_person, nouns)
 
         for chars in range(0, int(arguments['--count'])):
             print (' '.join([pick(adjs) for x in range(0, int(arguments['--adjectives']))]) + 
-                    ' ' + '-'.join([pick(nns) for x in range(0, int(arguments['--nouns']))]) )
+                    ' ' + '-'.join([pick(people) for x in range(0, int(arguments['--nouns']))]) )
 
     else:
         print "getting locs"
         # Get nouns that are locations
         is_place = lambda x: has_hypernym(x,'location') or has_hypernym(x,'structure')
         locs = filter(is_place, nouns)
-        locs = discard_common(locs,.1)
 
         # Get events
         is_event = lambda x: has_hypernym(x,'event')
         events = filter(is_event, nouns)
-        events = discard_common(events,.1)
         
         for chars in range(0, int(arguments['--count'])):
             print (
